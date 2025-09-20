1b80593031bf3266bb7f6654d857e50341e619e0
DAI-ai-code <ajcs2508@gmail.com>
2025-09-20 11:56:53 +0530

    commit

--- days/day11/employee_db_prac/better_emp_operations.py
+++ days/day11/employee_db_prac/better_emp_operations.py
@@ -7,15 +7,15 @@ cursor = con.cursor()
 
 def inserter(eid,name,salary):
     cursor.execute("insert into employee values({},'{}',{})".format(eid,name,salary))
-    
+    select_all()
 
 def deleter(eid):
     cursor.execute(f"delete from employee where id={eid}")
-    
+    select_all()
 
 def updater(eid, name, salary):
     cursor.execute(f"update employee set name='{name}' where id={eid}")
-    
+    select_all()
 
 def selector(eid, name, salary):
     cursor.execute(f"select * from employee where salary>{salary}")
@@ -29,14 +29,14 @@ def select_all():
     print(data)
 
 
-e1 = Employee(1,"abc",99000)
-e2 = Employee(2,"bcd", 89000)
-e3 = Employee(3,"cde",90000)
-e4 = Employee(4,"def", 100000)
+e1 = Employee(5,"abc",99000)
+e2 = Employee(6,"bcd", 89000)
+e3 = Employee(7,"cde",90000)
+e4 = Employee(8,"def", 100000)
 
 e = [e1,e2,e3,e4]
-# for emp in e:
-#     inserter(emp.empid, emp.ename, emp.esalary)
+for emp in e:
+    inserter(emp.empid, emp.ename, emp.esalary)
 
 select_all()
 selector(1,"a",90000)
